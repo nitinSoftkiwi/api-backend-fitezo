c603b52b034263a55d10803e8847b08a9928431c
nitinSoftkiwi <nitin@softkiwi.co.in>
2022-04-02 17:36:25 +0530

    add hidden some part

--- index.js
+++ index.js
@@ -51,30 +51,30 @@ app.use('/api', userRoutes);
 app.get('/',(req , res) => {
     res.send('Hi Fitezo')
 })
-console.log('nirninvrnivnr',  "'" +process.env.SMTP_TO_EMAIL+ "'");
-const contactEmail = {
-    //this is the authentication for sending email.
-host: 'smtp.gmail.com',
-// port: 465,
-port: 587,
-secure: true, // use TLS
-//create a .env file and define the process.env variables 
-   // with the credentials you want to use for your email server.
-auth: {
-    Username: "'" +process.env.SMTP_TO_EMAIL+ "'",
-    Password: "'" +process.env.SMTP_TO_PASSWORD+ "'",
-},
+// console.log('nirninvrnivnr',  "'" +process.env.SMTP_TO_EMAIL+ "'");
+// const contactEmail = {
+//     //this is the authentication for sending email.
+// host: 'smtp.gmail.com',
+// // port: 465,
+// port: 587,
+// secure: true, // use TLS
+// //create a .env file and define the process.env variables 
+//    // with the credentials you want to use for your email server.
+// auth: {
+//     Username: "'" +process.env.SMTP_TO_EMAIL+ "'",
+//     Password: "'" +process.env.SMTP_TO_PASSWORD+ "'",
+// },
 
-}
+// }
 
-const transporter = nodemailer.createTransport(contactEmail)
-    transporter.verify((error, success) => {
-if (error) {
-    //if error happened code ends here
-    console.error('niyniyninynyniyn',error)
-} else {
-    //this means success
-    console.log('Ready to send mail!',success)
-}
-})
+// const transporter = nodemailer.createTransport(contactEmail)
+//     transporter.verify((error, success) => {
+// if (error) {
+//     //if error happened code ends here
+//     console.error('niyniyninynyniyn',error)
+// } else {
+//     //this means success
+//     console.log('Ready to send mail!',success)
+// }
+// })
 app.listen(process.env.PORT,() => console.log("server is running on Port ::: "+process.env.PORT));
